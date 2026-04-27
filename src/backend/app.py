c4713e9a8596a8bd6a7547d3792b1f8aed594c9e
soap2785 <ultramarinow@gmail.com>
2026-04-27 20:31:32 +0300

    fixed including deployment router

--- src/backend/app.py
+++ src/backend/app.py
@@ -6,7 +6,7 @@ from views.books import books_router
 from views.deployment import deployment_router
 
 app = FastAPI()
+app.include_router(deployment_router)
 app.include_router(audiobooks_router)
 app.include_router(authors_router)
 app.include_router(books_router)
-app.include_router(deployment_router)
